acca8caf88433c6a4d5331621534d5dada4910d9
SherninDI <mitya7144@gmail.com>
2023-11-20 04:13:55 +0300

    done

--- app/src/main/java/com/example/kp/MessageIn.java
+++ app/src/main/java/com/example/kp/MessageIn.java
@@ -2,7 +2,6 @@ package com.example.kp;
 
 public class MessageIn extends Messages {
     public  MessageIn(String message){
-        super(message);    // если базовый класс определяет конструктор
-        //  то производный класс должен его вызвать
+        super(message);
     }
 }
--- app/src/main/java/com/example/kp/MessageOut.java
+++ app/src/main/java/com/example/kp/MessageOut.java
@@ -2,7 +2,7 @@ package com.example.kp;
 
 public class MessageOut extends Messages {
     public  MessageOut(String message){
-        super(message);    // если базовый класс определяет конструктор
-        //  то производный класс должен его вызвать
+        super(message);
+
     }
 }
